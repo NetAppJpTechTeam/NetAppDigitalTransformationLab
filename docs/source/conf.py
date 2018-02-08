241527cd36124feb9b4a361b53d0b61da04e5cb9
Makoto Watanabe <makoto.wata@gmail.com>
2018-02-08 11:43:42 +0900

    Enabled todolist.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,9 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
+[extensions]
+todo_include_todos = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
