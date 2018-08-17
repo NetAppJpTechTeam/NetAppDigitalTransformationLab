ee22d500867b1dba310a519249abbba83f43c681
Makoto Watanabe <makoto.wata@gmail.com>
2018-08-17 18:56:35 +0900

    Update docker-sphinx version.

--- docs/utils/autobuild.sh
+++ docs/utils/autobuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 127.0.0.1:8000:8000 -v "$(pwd)":/docs --rm -d  makotow/sphinx-docker:1.2-sphinx1.7.2 sphinx-autobuild -H 0.0.0.0 source/ build/html
+docker run -p 127.0.0.1:8000:8000 -v "$(pwd)":/docs --rm -d  makotow/sphinx-docker:1.3-sphinx1.7.6 sphinx-autobuild -H 0.0.0.0 source/ build/html
--- docs/utils/clean.sh
+++ docs/utils/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v "$(pwd)":/docs --rm -it makotow/sphinx-docker:1.1 make clean
+docker run -v "$(pwd)":/docs --rm -it makotow/sphinx-docker:1.3-sphinx1.7.6make clean
